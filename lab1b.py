ed536532bcfc1d9e2f4daab405ade7f44a823f7c
Sahej Sharma <ssharma478@myseneca.ca>
2024-05-17 14:33:24 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Create a string object called "name" with the value "Isaac"
+name = 'Isaac'
+
+# Print the string object "name" along with a question
+print('How old are you ' + name + '?')
+
+
