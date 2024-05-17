8364a106f68f470f2eb99ac87253095aeb4e0db0
Sahej Sharma <ssharma478@myseneca.ca>
2024-05-17 14:40:53 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Create two integer objects, num1 and num2
+num1 = 5
+num2 = 10
+
+# Create a new object called "sum" and perform math
+sum_result = num1 + num2
+
+# Define name and age objects
+name = 'Isaac'
+age = 72
+
+# Print the statement combining string and integer objects
+print(name + ' is ' + str(age) + ' years old!')
+
