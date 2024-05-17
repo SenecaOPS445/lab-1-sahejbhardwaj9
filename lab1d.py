d97a6b5511bf30710e2ab91bb73481e7205f6b00
Sahej Sharma <ssharma478@myseneca.ca>
2024-05-17 14:44:32 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Define integer objects x, y, and z
+x = 10
+y = 2
+z = 5
+
+# Perform the math operation and print the result
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
+
